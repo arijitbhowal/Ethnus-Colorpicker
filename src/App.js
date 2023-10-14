bfe217bb5a35ec1bab5f99008c99a27321c53b41
arijitbhowal <arijitaeiou2002@gmail.com>
2023-10-14 20:37:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ColorPicker from "./ColorPicker";
+import "./App.css"; // Make sure to import the CSS for App component
 
 function App() {
+  const colors = [
+    "red",
+    "green",
+    "blue",
+    "yellow",
+    "purple",
+    "orange",
+    "pink",
+    "teal",
+    "cyan",
+    "lime",
+    "brown",
+    "gray",
+    "black",
+    "maroon",
+    "navy",
+    "olive",
+    "fuchsia",
+    "aqua",
+    "silver",
+    "gold",
+    "indigo",
+    "violet",
+    "magenta",
+    "turquoise",
+    "plum",
+    "salmon",
+    "coral",
+    "lavender",
+    "peru",
+    "orchid",
+    "sienna",
+    "tan",
+    "khaki",
+    "chartreuse",
+    "thistle",
+    "crimson",
+    "tomato",
+    "royalblue",
+    "darkgreen",
+    "slategray",
+    "chocolate",
+    "firebrick",
+    "steelblue",
+    "seagreen",
+    "darkorchid",
+    "darkgoldenrod",
+    "rosybrown",
+    "cadetblue",
+    "slateblue",
+    "springgreen",
+    "forestgreen",
+    "powderblue",
+    "darkviolet",
+    "mediumturquoise",
+    "mediumseagreen",
+    "dodgerblue",
+    "darkslategray",
+    "saddlebrown",
+    "mediumvioletred",
+    "dimgray",
+    "burlywood",
+    "navajowhite",
+    "lightcoral",
+    "goldenrod",
+    "greenyellow",
+    "lightseagreen",
+    "lightsteelblue",
+    "palevioletred",
+    "mediumaquamarine",
+    "cadetblue",
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <h2>Color Picker</h2>
+      <div className="color-picker-container">
+        <ColorPicker colors={colors} />
+      </div>
     </div>
   );
 }
